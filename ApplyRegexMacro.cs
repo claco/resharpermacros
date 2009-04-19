82ab3aefb3032178d138d3db7f43942087b32820
Christopher H. Laco <claco@chrislaco.com>
2009-04-19 18:42:37 -0400

    Initial version of my ReSharper Macros collection.

---
+++ ApplyRegexMacro.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Macros;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Hotspots;
+
+namespace ChrisLaco.ReSharper.Macros
+{
+    [Macro("applyRegex", ShortDescription = "Replace string matching {#0:pattern} with {#1:string} in {#2:variable}", LongDescription = "Applies regex to replace value in another template variable.")]
+    class ApplyRegexMacro : IMacro
+    {
+        #region IMacro Members
+
+        public string EvaluateQuickResult(IHotspotContext context, IList<string> arguments)
+        {
+            if (arguments.Count != 3)
+            {
+                return null;
+            }
+            else
+            {
+                try
+                {
+                    var result = Regex.Replace(arguments[2], arguments[0], arguments[1], RegexOptions.IgnoreCase);
+
+                    return result;
+                }
+                catch (Exception e)
+                {
+                    return "<" + e.Message + ">";
+                }
+            }
+        }
+
+        public HotspotItems GetLookupItems(IHotspotContext context, IList<string> arguments)
+        {
+            return null;
+        }
+
+        public string GetPlaceholder()
+        {
+            return "a";
+        }
+
+        public bool HandleExpansion(IHotspotContext context, IList<string> arguments)
+        {
+            return false;
+        }
+
+        public ParameterInfo[] Parameters
+        {
+            get
+            {
+                return new[] { new ParameterInfo(ParameterType.String), new ParameterInfo(ParameterType.String), new ParameterInfo(ParameterType.VariableReference) };           
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using JetBrains.UI.Application.PluginSupport;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ReSharper Macros")]
+[assembly: AssemblyDescription("Additional template macros for ReSharper 4.5.x")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Christopher H. Laco")]
+[assembly: AssemblyProduct("ReSharper Macros")]
+[assembly: AssemblyCopyright("Copyright © 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: PluginTitle("ChrisLaco.ReSharper.Macros")]
+[assembly: PluginDescription("Provides additional template macros: ApplyRegexMacro.")]
+[assembly: PluginVendor("Christopher H. Laco")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4c3ca06c-b003-4cb9-9bbd-2b9602c45bdc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("4.5.0.0")]
+[assembly: AssemblyFileVersion("4.5.0.0")]
